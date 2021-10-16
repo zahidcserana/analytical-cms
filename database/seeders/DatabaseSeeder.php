d1bfbff5312b03bbe3c0fe8f30c94697cfdf7ff0
Zahid <zahid1004048@gmail.com>
2021-10-16 10:35:46 +0600

    seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,9 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        DB::statement('SET FOREIGN_KEY_CHECKS=0');
+        // DB::statement('SET FOREIGN_KEY_CHECKS=0');
 
         DB::table('customers')->truncate();
         DB::table('invoice_items')->truncate();
@@ -29,6 +27,6 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class
         ]);
 
-        DB::statement('SET FOREIGN_KEY_CHECKS=1');
+        // DB::statement('SET FOREIGN_KEY_CHECKS=1');
     }
 }
